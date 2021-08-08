a4b6383809fd98b4a7e1572443f8b46af986e36f
tkarndbrtk <tkarndbrtk@naver.com>
2021-08-08 11:32:49 +0900

    2차실습
    1차세미나_2차실습

--- 1th_seminar/1th_seminar/ViewController.swift
+++ 1th_seminar/1th_seminar/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var phonenumberTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
     @IBAction func buttonOnClicked(_ sender: Any) {
-        titleLabel.text = "변경완료!"
-        self.titleLabel.textColor = UIColor.red
-        titleLabel.textAlignment = .center
+        if phonenumberTextField.text?.isEmpty == false && nameTextField.text?.isEmpty == false {
+            titleLabel.text = "\(nameTextField.text!)의 번호는 \(phonenumberTextField.text!)입니다!"
+        
+            titleLabel.textAlignment = .center
+
+        }
+        else {
+            titleLabel.text = ""
+        }
     }
     override func viewDidLoad() {
+        nameTextField.clearButtonMode = .whileEditing
+        nameTextField.keyboardType = .default
+    
+        phonenumberTextField.clearButtonMode = .whileEditing
+        phonenumberTextField.keyboardType = .phonePad
+        
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    
     }
 
 
 }
+//.️ textField에서 clear버튼은 텍스트를 작성하는 동안 노출 시켜주세요
+//2. 각 textField 별로 placeHolder를 이름/ 휴대폰 번호로 만들어주세요
+
+//3. 등록하기를 눌렀을 때, “(이름)의 번호는 (번호) 입니다!”
+//라는 문구가 Label에 표기되도록 해주세요!
+
+
 
